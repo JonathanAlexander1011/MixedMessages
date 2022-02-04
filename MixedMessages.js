72726f94aee4fb96eb766eaede1200a955e368ff
RazorSharp1011 <jonealexander1011.com>
2022-02-04 08:50:18 -0600

    Create variables to call array elements randomly

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,1 +1,10 @@
-console.log('hello');
+//Created arrays at varying lengths
+let who = ['I', 'You'];
+let preference = ['like', "don't like", 'LOVE', 'HATE'];
+let hobby = ['running', 'skiing', 'swimming', 'hiking', 'skating'];
+
+//Create variables to call elements from each array randomly
+let randomWho = who[Math.floor(Math.random() * who.length)];
+let randomPreference = preference[Math.floor(Math.random() * preference.length)];
+let randomHobby = hobby[Math.floor(Math.random() * random.length)];
+
